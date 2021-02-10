5083dd77fdbba7aae262d272fdf999eaaebef494
Abhishek <Abhishek@WINDOWS-M6NEMET>
2021-02-09 22:27:17 -0500

    [ROSE- SAMPLE PROJECT]

---
+++ Test Suites/att.com launch.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>att.com launch</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>1b97d0c2-6bb0-4fb6-8cde-33dbecd7ef9a</testSuiteGuid>
+   <testCaseLink>
+      <guid>63ff0a25-3a14-496e-aa27-2df346bddfe7</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/attlaunch</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
